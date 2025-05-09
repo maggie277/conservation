cdf15b8e8738b87e697193eda96bf2e1a5d8c701
maggie <margretsatyele@gmail.com>
2025-05-10 01:53:02 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Navbar.js 	modified:   src/components/navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
+  const [click, setClick] = useState(false);
   const hideNavbarPaths = ['/', '/login', '/register'];
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   if (hideNavbarPaths.includes(location.pathname)) {
     return null;
@@ -13,25 +17,44 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">TerraFund Zambia</Link>
-        <ul className="nav-menu">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          TerraFund Zambia
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <div className={click ? 'menu-bar change' : 'menu-bar'}></div>
+          <div className={click ? 'menu-bar change' : 'menu-bar'}></div>
+          <div className={click ? 'menu-bar change' : 'menu-bar'}></div>
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/projects" className="nav-links">Farm Projects</Link>
+            <Link to="/projects" className="nav-links" onClick={closeMobileMenu}>
+              Farm Projects
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/farmers-forum" className="nav-links">Farmers Forum</Link>
+            <Link to="/farmers-forum" className="nav-links" onClick={closeMobileMenu}>
+              Farmers Forum
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/projects/upload-project" className="nav-links">Submit Project</Link>
+            <Link to="/projects/upload-project" className="nav-links" onClick={closeMobileMenu}>
+              Submit Project
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/help-center" className="nav-links">Help Center</Link>
+            <Link to="/help-center" className="nav-links" onClick={closeMobileMenu}>
+              Help Center
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/profile" className="nav-links">My Farm</Link>
+            <Link to="/profile" className="nav-links" onClick={closeMobileMenu}>
+              My Farm
+            </Link>
           </li>
         </ul>
       </div>
